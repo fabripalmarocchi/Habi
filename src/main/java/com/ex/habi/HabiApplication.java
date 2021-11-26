c82318b9a2fa549252a28b97153872d5580fdd59
fabripalmarocchi <82984205+fabripalmarocchi@users.noreply.github.com>
2021-11-26 17:06:18 -0300

    Add files via upload

---
+++ src/main/java/com/ex/habi/HabiApplication.java
@@ -0,0 +1,13 @@
+package com.ex.habi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex/habi/HabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.habi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
